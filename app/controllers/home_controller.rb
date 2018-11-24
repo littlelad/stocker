c51448a543673d203cce5d6104d68431ae45ffa9
Joel <joel0822@gmail.com>
2018-11-24 14:49:27 +0300

    setting up stock price

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
   def index
+    @error = "Symbol not exist, please try again"
+    if params[:id] == ""
+      @nothing = "Hey, don't forget to enter a symbol"
+    elsif 
+      if params[:id]
+        begin
+          @stock =  StockQuote::Stock.quote(params[:id])
+        rescue
+          @error
+        end
+
+
+      end
+    end
   end
   
   def about
+
   end
 end
